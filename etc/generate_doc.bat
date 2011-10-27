4ce909995407c3ea27af64a67faf429f3106a1aa
Malachi Griffie <malachi@nexussays.com>
2011-10-27 03:09:02 -0700

    moved random batch files from / to /etc/

---
+++ etc/generate_doc.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+REM rmdir ".\doc" /s /q
+"C:\develop\sdk\flex_sdk_4.5.1.21328\bin\asdoc.exe" -source-path "..\src" -doc-sources "..\src" -main-title "nexuslib AS3 Library" -output "..\doc" -lenient -warnings 
+PAUSE
--- generate_doc.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-REM rmdir ".\doc" /s /q
-"C:\develop\sdk\flex_sdk_4.5.1.21328\bin\asdoc.exe" -source-path "src" -doc-sources "src" -main-title "nexuslib AS3 Library" -output "doc" -lenient -warnings 
-PAUSE
