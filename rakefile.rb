cdb8f0ac048ea4a317dd31a00bb1ae042e6125ac
Malachi Griffie <malachi@nexussays.com>
2012-07-05 00:02:03 -0700

    Updated build script

--- rakefile.rb
+++ rakefile.rb
@@ -1,8 +1,5 @@
-require 'rubygems'
-require 'bundler/setup'
-
-require './build/lib/asrake'
-#require 'C:\Users\nexus\Development\Projects\Personal\ASRake\lib\asrake.rb'
+gem 'asrake', ">=0.7"
+require 'asrake'
 
 FlexSDK::SDK_PATHS << 'C:\develop\sdk\flex_sdk_4.6.0.23201'
 
@@ -34,18 +31,18 @@ task :clobber
 		version_file = "#{root}/VERSION"
 		version = "#{Version.current(version_file) || '0.0.0'}"
 
-		compc = ASRake::Compc.new
+		compc = ASRake::CompcArguments.new
 		compc.target_player = 11.0
 		compc.output = "#{root}/bin/#{proj}.swc"
-		#compc.debug = true
+		compc.debug = true
 		compc.source_path << "#{root}/src"
 		compc.statically_link_only_referenced_classes << "lib/blooddy_crypto_0.3.5/blooddy_crypto.swc"
 		#compc.dump_config = "#{root}/src/compc_config.xml"
 		
 		desc "Build nexuslib.#{project}"
-		ASRake::SWCTask.new :build, compc
+		ASRake::CompcTask.new :build, compc
 
-		desc "Package the project into a zip"
+		desc "Package #{proj}-#{version}.zip"
 		ASRake::PackageTask.new :package => :build do |package|
 			package.output = "#{compc.output_dir}/#{proj}-#{version}.zip"
 			package.files = {
