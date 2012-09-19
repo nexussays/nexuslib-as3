cdadd1c9a3bcb56edff2ef24bdd42d937a765ca4
Malachi Griffie <malachi@nexussays.com>
2012-09-19 01:54:46 -0700

    Updated build scripts for ASRake 0.11.0

--- etc/generate_doc.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-rmdir "..\doc" /s /q
-"C:\develop\sdk\flex_sdk_4.6.0.23201\bin\asdoc.exe" -doc-sources "..\projects\reflection\src" -source-path "..\projects\reflection\src" -library-path "..\lib\blooddy_crypto_0.3.5\blooddy_crypto.swc" -main-title "nexuslib AS3 Library" -output "..\doc" -lenient -warnings 
-PAUSE
--- rakefile.rb
+++ rakefile.rb
@@ -1,32 +1,54 @@
-gem 'asrake', ">=0.10"
+gem 'asrake', ">=0.11"
 require 'asrake'
 require 'rake/clean'
 
 FlexSDK::SDK_PATHS << 'C:\develop\sdk\flex_sdk_4.6.0.23201'
 
-#############################################
-# Tasks
-#############################################
+#
+# Setup
+#
 
-task :default => :build
+PROJECTS = %w{reflection enigma mercury}
+
+doc = ASRake::Asdoc.new
+doc.output = "bin/doc/"
+doc.window_title = "\"nexuslib API Documentation\""
+CLEAN.include(doc.output)
+
+doc_footer = []
+
+#
+# Tasks
+#
+
+task :default => :build_all
 
 desc "Build all projects"
-task :build
+task :build_all
 
 desc "Package all projects"
 task :package
 
 desc "Generate docs for all projects"
 task :doc do
-	@doc.execute
+	doc.footer = doc_footer.join("/")
+	doc.execute
 end
-@doc = ASRake::Asdoc.new
-@doc.output = "bin/doc/"
 
-CLEAN.include(@doc.output)
+#
+# Generate Project Tasks
+#
 
-%w{reflection enigma}.each do |project|
+PROJECTS.each do |project|
 	project = project.to_sym
+
+	# default build task to namespace name
+	desc "Build nexuslib.#{project}"
+	task project => "#{project}:build"
+	
+	# add to root tasks
+	task :build_all => "#{project}:build"
+	task :package => "#{project}:package"
 
 	namespace project do
 
@@ -34,17 +56,23 @@ CLEAN.include(@doc.output)
 		proj = "nexuslib.#{project}"
 		version_file = "#{root}/VERSION"
 		version = "#{Version.current(version_file) || '0.0.0'}"
+		# add version info to doc_footer of docs
+		doc_footer << "#{proj}-#{version}"
 
-		desc "Build nexuslib.#{project}"
-		build = ASRake::CompcTask.new :build do |compc|
-			compc.target_player = 11.0
-			compc.output = "bin/#{proj}.swc"
-			compc.debug = true
-			compc.source_path << "#{root}/src"
-			compc.statically_link_only_referenced_classes << "lib/blooddy_crypto_0.3.5/blooddy_crypto.swc"
-			compc.include_asdoc = true
-			#compc.dump_config = "#{root}/src/compc_config.xml"
-		end
+		build = ASRake::CompcArguments.new
+		build.target_player = 11.0
+		build.output = "bin/#{proj}.swc"
+		build.debug = true
+		build.source_path << "#{root}/src"
+		build.statically_link_only_referenced_classes << "lib/blooddy_crypto_0.3.5/blooddy_crypto.swc"
+		build.include_asdoc = true
+		#build.dump_config = "#{root}/src/compc_config.xml"
+		CLEAN.include(build.output)
+
+		ASRake::CompcTask.new :build, build
+
+		# add paths to asdoc task
+		doc.add build
 
 		desc "Package #{proj}-#{version}.zip"
 		package = ASRake::PackageTask.new :package => :build do |package|
@@ -54,21 +82,10 @@ CLEAN.include(@doc.output)
 				"#{proj}-#{version}.swc" => build.output
 			}
 		end
-
-		ASRake::VersionTask.new :version, version_file
-
-		# add paths to asdoc task
-		@doc.source_path << build.source_path
-		@doc.library_path << build.library_path
-		@doc.library_path << build.include_libraries
-
-		# clean
-		CLEAN.include(build.output)
 		CLOBBER.include(package.output)
 
+		ASRake::VersionTask.new :version, version_file
+		
 	end
 
-	#add to root tasks
-	task :build => "#{project}:build"
-	task :package => "#{project}:package"
 end
