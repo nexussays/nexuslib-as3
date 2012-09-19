a2cfc8a1d69567899614d7c1f00053d8d0c86a5e
Malachi Griffie <malachi@nexussays.com>
2012-09-19 03:04:05 -0700

    Updated build script, added deploy task to put API docs on S3

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,8 @@
 gem 'asrake', ">=0.11"
+gem 'right_aws', ">=3.0"
 require 'asrake'
 require 'rake/clean'
+require 'right_aws'
 
 FlexSDK::SDK_PATHS << 'C:\develop\sdk\flex_sdk_4.6.0.23201'
 
@@ -11,7 +13,7 @@ FlexSDK::SDK_PATHS << 'C:\develop\sdk\flex_sdk_4.6.0.23201'
 PROJECTS = %w{reflection enigma mercury}
 
 doc = ASRake::Asdoc.new
-doc.output = "bin/doc/"
+doc.output = "bin/doc"
 doc.window_title = "\"nexuslib API Documentation\""
 CLEAN.include(doc.output)
 
@@ -24,7 +26,7 @@ doc_footer = []
 task :default => :build_all
 
 desc "Build all projects"
-task :build_all
+multitask :build_all
 
 desc "Package all projects"
 task :package
@@ -33,6 +35,28 @@ desc "Generate docs for all projects"
 task :doc do
 	doc.footer = doc_footer.join("/")
 	doc.execute
+end
+
+multitask :deploy
+desc "Deploy"
+task :deploy, [:key, :secret_key] => [:package, :doc] do |t, args|
+	aws_secret_key = args[:secret_key]
+	aws_key = args[:key]
+	
+	fail "Must provide AWS keys" if aws_secret_key == nil || aws_key == nil
+
+	s3 = RightAws::S3.new(aws_key, aws_secret_key)
+	docs = s3.bucket('docs.nexussays.com')
+	#docs.delete_folder('nexuslib')
+	Dir.chdir(doc.output) do
+		Dir.glob("**/*") do |file|
+			if !File.directory?(file)
+				key = File.join('nexuslib', file)
+				puts key
+				docs.put(key, File.open(file), {}, 'public-read')
+			end
+		end
+	end
 end
 
 #
@@ -85,7 +109,7 @@ PROJECTS.each do |project|
 		CLOBBER.include(package.output)
 
 		ASRake::VersionTask.new :version, version_file
-		
+
 	end
 
 end
