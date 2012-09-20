05fff90a794c2f6269f4b61bcbcc362722eb49f0
Malachi Griffie <malachi@nexussays.com>
2012-09-20 01:56:29 -0700

    Update build task for ASRake 1.0.0 release

--- rakefile.rb
+++ rakefile.rb
@@ -1,4 +1,4 @@
-gem 'asrake', ">=0.11"
+gem 'asrake', ">=1.0"
 gem 'right_aws', ">=3.0"
 require 'asrake'
 require 'rake/clean'
@@ -12,12 +12,12 @@ FlexSDK::SDK_PATHS << 'C:\develop\sdk\flex_sdk_4.6.0.23201'
 
 PROJECTS = %w{reflection enigma mercury}
 
-doc = ASRake::Asdoc.new
-doc.output = "bin/doc"
-doc.window_title = "\"nexuslib API Documentation\""
-CLEAN.include(doc.output)
+asdoc = ASRake::Asdoc.new
+asdoc.output = "bin/doc"
+asdoc.window_title = "\"nexuslib API Documentation\""
+CLEAN.include(asdoc.output)
 
-doc_footer = []
+asdoc_footer = []
 
 #
 # Tasks
@@ -33,26 +33,28 @@ task :package
 
 desc "Generate docs for all projects"
 task :doc do
-	doc.footer = doc_footer.join("/")
-	doc.execute
+	asdoc.footer = asdoc_footer.join("/")
+	asdoc.execute
 end
 
 multitask :deploy
+
 desc "Deploy"
 task :deploy, [:key, :secret_key] => [:package, :doc] do |t, args|
-	aws_secret_key = args[:secret_key]
-	aws_key = args[:key]
+	aws_secret_key = args[:secret_key].chomp
+	aws_key = args[:key].chomp
 	
 	fail "Must provide AWS keys" if aws_secret_key == nil || aws_key == nil
 
 	s3 = RightAws::S3.new(aws_key, aws_secret_key)
 	docs = s3.bucket('docs.nexussays.com')
 	#docs.delete_folder('nexuslib')
-	Dir.chdir(doc.output) do
+	Dir.chdir(asdoc.output) do
 		Dir.glob("**/*") do |file|
 			if !File.directory?(file)
 				key = File.join('nexuslib', file)
 				puts key
+				# TODO: Only put if source is newer than destination
 				docs.put(key, File.open(file), {}, 'public-read')
 			end
 		end
@@ -63,53 +65,52 @@ end
 # Generate Project Tasks
 #
 
-PROJECTS.each do |project|
-	project = project.to_sym
+PROJECTS.each do |name|
 
-	# default build task to namespace name
-	desc "Build nexuslib.#{project}"
-	task project => "#{project}:build"
-	
-	# add to root tasks
-	task :build_all => "#{project}:build"
-	task :package => "#{project}:package"
+	name = name.to_sym
+	root = "projects/#{name}"
+	project = "nexuslib.#{name}"
+	version_file = "#{root}/VERSION"
+	version = "#{Version.current(version_file) || '0.0.0'}"
 
-	namespace project do
+	# create tasks to build the swc
+	build = ASRake::Compc.new "bin/#{project}.swc"
+	build.target_player = 11.0
+	build.debug = true
+	build.source_path << "#{root}/src"
+	build.statically_link_only_referenced_classes << "lib/blooddy_crypto_0.3.5/blooddy_crypto.swc"
+	build.include_asdoc = true
+	#build.dump_config = "#{root}/src/compc_config.xml"
+	CLEAN.include(build.output)
 
-		root = "projects/#{project}"
-		proj = "nexuslib.#{project}"
-		version_file = "#{root}/VERSION"
-		version = "#{Version.current(version_file) || '0.0.0'}"
-		# add version info to doc_footer of docs
-		doc_footer << "#{proj}-#{version}"
+	# create task to package into a zip file
+	package = ASRake::Package.new "#{build.output_dir}/#{project}-#{version}.zip"
+	package.files = {
+		"license.txt" => "LICENSE",
+		"#{project}-#{version}.swc" => build.output
+	}
+	CLOBBER.include(package.output)
 
-		build = ASRake::CompcArguments.new
-		build.target_player = 11.0
-		build.output = "bin/#{proj}.swc"
-		build.debug = true
-		build.source_path << "#{root}/src"
-		build.statically_link_only_referenced_classes << "lib/blooddy_crypto_0.3.5/blooddy_crypto.swc"
-		build.include_asdoc = true
-		#build.dump_config = "#{root}/src/compc_config.xml"
-		CLEAN.include(build.output)
+	# add to asdoc
+	asdoc.add(build)
+	# add version info to asdoc_footer of docs
+	asdoc_footer << "#{project}-#{version}"
 
-		ASRake::CompcTask.new :build, build
+	namespace name do
 
-		# add paths to asdoc task
-		doc.add build
-
-		desc "Package #{proj}-#{version}.zip"
-		package = ASRake::PackageTask.new :package => :build do |package|
-			package.output = "#{build.output_dir}/#{proj}-#{version}.zip"
-			package.files = {
-				"license.txt" => "LICENSE",
-				"#{proj}-#{version}.swc" => build.output
-			}
-		end
-		CLOBBER.include(package.output)
+		desc "Package #{project}-#{version}.zip"
+		task :package => [build, package]
 
 		ASRake::VersionTask.new :version, version_file
 
 	end
+
+	# default build task to namespace name
+	desc "Build nexuslib.#{name}"
+	task name => build
+	
+	# add to root tasks
+	task :build_all => build
+	task :package => "#{name}:package"
 
 end
