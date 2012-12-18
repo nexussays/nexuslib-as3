0bced0382b4e841a3bb63f0c96f9e6639a357e9b
Malachi Griffie <malachi@nexussays.com>
2012-12-18 02:52:55 -0800

    Updated deployment task to upload zip to s3

--- rakefile.rb
+++ rakefile.rb
@@ -10,30 +10,31 @@ FlexSDK::SDK_PATHS << 'C:\develop\sdk\flex_sdk_4.6.0.23201'
 # Setup
 #
 
+project_name = "nexuslib"
 version = "#{Version.current || '0.0.0'}"
 
-compc = ASRake::Compc.new "bin/nexuslib.swc"
-compc.target_player = 11.0
-compc.debug = false
-compc.source_path << "src"
-compc.statically_link_only_referenced_classes << "lib/blooddy_crypto_0.3.5/blooddy_crypto.swc"
-compc.include_asdoc = true
-#compc.dump_config = "compc_config.xml"
-CLEAN.add compc
+nexuslib = ASRake::Compc.new "bin/#{project_name}.swc"
+nexuslib.target_player = 11.0
+nexuslib.debug = false
+nexuslib.source_path << "src"
+nexuslib.statically_link_only_referenced_classes << "lib/blooddy_crypto_0.3.5/blooddy_crypto.swc"
+nexuslib.include_asdoc = true
+#nexuslib.dump_config = "compc_config.xml"
+CLEAN.add(nexuslib)
 
 # create task to package into a zip file
-zip = ASRake::Package.new "#{compc.output_dir}/nexuslib-#{version}.zip"
+zip = ASRake::Package.new "#{nexuslib.output_dir}/#{project_name}-#{version}.zip"
 zip.files = {
 	"license.txt" => "LICENSE",
-	"nexuslib-#{version}.swc" => compc.output
+	"#{project_name}-#{version}.swc" => nexuslib.output
 }
-CLOBBER.add zip.output
+CLOBBER.add(zip.output)
 
 asdoc = ASRake::Asdoc.new "bin/doc"
-asdoc.window_title = "\"nexuslib API Documentation\""
-asdoc.footer = "nexuslib-#{version}"
-asdoc.add compc
-CLEAN.add asdoc
+asdoc.window_title = "\"#{project_name} API Documentation\""
+asdoc.footer = "#{project_name}-#{version}"
+asdoc.add(nexuslib)
+CLEAN.add(asdoc)
 
 #
 # Tasks
@@ -43,8 +44,8 @@ task :default do
 	system "rake --tasks"
 end
 
-desc "Build nexuslib"
-task :build => compc
+desc "Build #{project_name}"
+task :build => nexuslib
 
 desc "Package zip"
 task :package => [:build, zip]
@@ -54,21 +55,27 @@ ASRake::VersionTask.new :version
 desc "Generate docs"
 task :doc => asdoc
 
-namespace :doc do
-	desc "Deploy docs to S3"
-	task :deploy, [:key, :secret_key] => [:doc] do |t, args|
-		s3 = RightAws::S3.new(args[:key], args[:secret_key])
-		docs = s3.bucket('docs.nexussays.com')
-		#docs.delete_folder('nexuslib')
-		Dir.chdir(asdoc.output) do
-			Dir.glob("**/*") do |file|
-				if !File.directory?(file)
-					key = File.join('nexuslib', file)
-					puts key
-					# TODO: Only put if source is newer than destination
-					docs.put(key, File.open(file), {}, 'public-read')
-				end
+desc "Deploy zip & docs to S3"
+task :deploy, [:key, :secret_key] => [:package, :doc] do |t, args|
+	s3 = RightAws::S3.new(args[:key], args[:secret_key])
+
+	# documentation
+	docs_bucket = s3.bucket('docs.nexussays.com')
+	#docs_bucket.delete_folder('nexuslib')
+	Dir.chdir(asdoc.output) do
+		Dir.glob("**/*") do |file|
+			if !File.directory?(file)
+				key = File.join(project_name, file)
+				puts "#{docs_bucket.name}/#{key}"
+				# TODO: Only put if source is newer than destination
+				docs_bucket.put(key, File.open(file), {}, 'public-read')
 			end
 		end
 	end
+
+	# zip
+	pkg_bucket = s3.bucket('public.nexussays.com')
+	key = File.join("code", project_name, zip.output_file)
+	puts "#{pkg_bucket.name}/#{key}"
+	pkg_bucket.put(key, File.open(zip.output), {}, 'public-read')
 end
